7466746dea51bc12b396f627431fe80186f59d1b
lxzr <52447390+lxzr@users.noreply.github.com>
2019-07-12 16:14:06 +0800

    Update a1.java

--- 第一天/src/firstday/a1.java
+++ 第一天/src/firstday/a1.java
@@ -6,12 +6,13 @@ import java.util.Map;
 
 public class a1 {
 public static void main(String[] args) {
-	Map<String , String> map=new HashMap<String, String>();
-	map.put("kye1","1");
-	map.put("key2","2");
-	map.put("key3","3");
-	for( Map.Entry<String, String> entry : map.entrySet()) {
-		System.out.println(entry.getKey()+"="+entry.getValue());
-	}
+	//Map<String , String> map=new HashMap<String, String>();
+	//map.put("kye1","1");
+	//map.put("key2","2");
+	//map.put("key3","3");
+	//for( Map.Entry<String, String> entry : map.entrySet()) {
+	//	System.out.println(entry.getKey()+"="+entry.getValue());
+	//}
+	System.out.print("你好");
 }
 }
